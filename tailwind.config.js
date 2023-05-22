96f2733c5c4af013df694c252d959d77a86411da
7-18 <kevinbrianbr@gmail.com>
2023-05-22 18:31:33 -0500

    feat: add new classes to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "navigation-hover":
+          "linear-gradient(180deg, #231F20 0%, rgba(34, 34, 34, 0) 100%)",
+        "progress-bar":
+          "linear-gradient(180deg, #31969D 0%, #65E6F4 31.77%, #24AEBD 66.15%, #1299A8 98.96%)",
+      },
+      backgroundColor: {
+        "content-card": "#0E0F0F",
+        "progress-card": "#1E2328",
+      },
+      textColor: {
+        gold: "#C89B3C",
+        progress: "#1DA5B4",
+      },
+      fontFamily: {
+        beaufort: ["Beaufort Regular", "sans-serif"],
+      },
+      boxShadow: {
+        progress:
+          "0px 0px 4px #FFA942, 0px 0px 4px rgba(255, 169, 66, 0.25), 0px 0px 7px rgba(255, 169, 66, 0.25)",
       },
     },
   },
   plugins: [],
-}
+};
