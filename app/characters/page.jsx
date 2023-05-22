ef7a873530bf7d5290ea8ce822dbeb60d39720b5
7-18 <kevinbrianbr@gmail.com>
2023-05-22 18:31:47 -0500

    feat: create characters page

---
+++ app/characters/page.jsx
@@ -0,0 +1,10 @@
+import { VideoCard } from "../components/VideoCard";
+
+export default function Characters() {
+  return (
+    <div className="container mx-auto">
+      <h1>Characters</h1>
+      <VideoCard />
+    </div>
+  );
+}
