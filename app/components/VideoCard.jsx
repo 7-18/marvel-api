1311b6e2a4854bb44ee7773574ac670cc630a19a
7-18 <kevinbrianbr@gmail.com>
2023-05-22 18:39:23 -0500

    feat: ceate VideoCard component without video

---
+++ app/components/VideoCard.jsx
@@ -0,0 +1,7 @@
+export const VideoCard = () => {
+  return (
+    <div className="w-[382px] bg-content-card rounded-md border-[#42331A] border-[0.7px]">
+      <h2>Aquí va un vídeo</h2>
+    </div>
+  );
+};
