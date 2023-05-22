780f43cc721e6c348f7457aaf87624f5118ca1b3
7-18 <kevinbrianbr@gmail.com>
2023-05-22 18:39:38 -0500

    feat: create ImageCard component without image

---
+++ app/components/ImageCard.jsx
@@ -0,0 +1,7 @@
+export const ImageCard = () => {
+  return (
+    <div className="w-[382px] bg-content-card rounded-md border-[#42331A] border-[0.7px]">
+      <h2>Aquí va una imagen</h2>
+    </div>
+  );
+};
