a5abda77d4e82d0b260d440f545878e33987a22b
7-18 <kevinbrianbr@gmail.com>
2023-05-23 02:13:15 -0500

    feat: add styles and video to Home

--- app/page.js
+++ app/page.js
@@ -1,7 +1,16 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Home</h1>
+    <main className="flex justify-center px-20">
+      <iframe
+        width={560}
+        height={500}
+        src="https://www.youtube.com/embed/UhVjp48U2Oc"
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+        className="w-full border-gold shadow-progress"
+        loading="lazy"
+      />
     </main>
-  )
+  );
 }
