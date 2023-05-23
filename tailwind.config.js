36bcb5f8a0a06f190b8ee8665a00c51365b60a77
7-18 <kevinbrianbr@gmail.com>
2023-05-23 02:07:23 -0500

    feat: add new classes and font Spiegel

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,22 +12,31 @@ module.exports = {
           "linear-gradient(180deg, #231F20 0%, rgba(34, 34, 34, 0) 100%)",
         "progress-bar":
           "linear-gradient(180deg, #31969D 0%, #65E6F4 31.77%, #24AEBD 66.15%, #1299A8 98.96%)",
+        "character-card":
+          "linear-gradient(360deg, #090C14 0%, #171A20 36.49%, #272C34 114.84%)",
       },
       backgroundColor: {
         "content-card": "#0E0F0F",
         "progress-card": "#1E2328",
+        gold: "#C89B3C",
       },
       textColor: {
         gold: "#C89B3C",
         progress: "#1DA5B4",
       },
       fontFamily: {
-        beaufort: ["Beaufort Regular", "sans-serif"],
+        beaufort: ["Beaufort Regular", "Spiegel"],
+        spiegel: ["Spiegel", "sans-serif"],
       },
       boxShadow: {
         progress:
           "0px 0px 4px #FFA942, 0px 0px 4px rgba(255, 169, 66, 0.25), 0px 0px 7px rgba(255, 169, 66, 0.25)",
+        'active-card':
+          "0px 0px 12px #FFA942, 0px 0px 12px rgba(255, 169, 66, 0.25), 0px 0px 10px rgba(255, 169, 66, 0.25)",
       },
+      borderColor: {
+        gold: "#C89B3C",
+      }
     },
   },
   plugins: [],
