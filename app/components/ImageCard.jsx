0ddc0b78f93e6ef840feebb942631612f989a27e
7-18 <kevinbrianbr@gmail.com>
2023-05-23 02:07:54 -0500

    feat: add an image to ImageCard

--- app/components/ImageCard.jsx
+++ app/components/ImageCard.jsx
@@ -1,7 +1,9 @@
+import Image from "next/image";
+
 export const ImageCard = () => {
   return (
-    <div className="w-[382px] bg-content-card rounded-md border-[#42331A] border-[0.7px]">
-      <h2>Aquí va una imagen</h2>
+    <div className="bg-content-card rounded-md border-[#42331A] border-[0.7px] w-full">
+      <Image alt="Infinity War Poster" src="/infinity-war.webp" width={382} height={192} className="w-full h-full" loading="lazy" />
     </div>
   );
 };
