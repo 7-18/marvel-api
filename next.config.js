cff90ad4b54a98272ff687d2d011c0405464b122
7-18 <kevinbrianbr@gmail.com>
2023-05-23 02:02:29 -0500

    feat: add api keys

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const nextConfig = {
+  env: {
+    MARVEL_PUBLIC_API_KEY: process.env.NEXT_PUBLIC_MARVEL_PUBLIC_KEY,
+    MARVEL_PRIVATE_API_KEY: process.env.NEXT_PUBLIC_MARVEL_PRIVATE_KEY,
+    MARVEL_API_HOST: "http://gateway.marvel.com",
+  },
+  images: {
+    domains: ["gateway.marvel.com", "i.annihil.us"],
+  },
+};
+
+module.exports = nextConfig;
