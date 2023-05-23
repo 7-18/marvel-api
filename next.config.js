7c9b63e851c53b1622a8661407d0acbbbdd035a8
7-18 <kevinbrianbr@gmail.com>
2023-05-23 02:34:36 -0500

    fix: cors problem in prod

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   env: {
     MARVEL_PUBLIC_API_KEY: process.env.NEXT_PUBLIC_MARVEL_PUBLIC_KEY,
     MARVEL_PRIVATE_API_KEY: process.env.NEXT_PUBLIC_MARVEL_PRIVATE_KEY,
-    MARVEL_API_HOST: "http://gateway.marvel.com",
+    MARVEL_API_HOST: "https://gateway.marvel.com",
   },
   images: {
     domains: ["gateway.marvel.com", "i.annihil.us"],
